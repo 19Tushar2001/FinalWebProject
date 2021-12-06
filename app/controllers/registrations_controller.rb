ae67f31be4abe7c1068281cb6763f0eabede4b6c
19Tushar2001 <19tusharsharma2001@gmail.com>
2021-12-06 15:15:14 -0600

    Adding Provice to Users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,10 @@
 class RegistrationsController < Devise:: RegistrationsController
   private
   def sign_up_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :address, :postal_code, :city)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :address, :postal_code, :city, :province_id)
   end
 
   def account_update_params
-    params.require(:user).permit(:name,:emial,:password,:password_confirmation,:address,:postal_code,:city)
+    params.require(:user).permit(:name,:emial,:password,:password_confirmation,:address,:postal_code,:city,:province_id)
   end
 end
